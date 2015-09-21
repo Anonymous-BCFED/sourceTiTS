cadf0f3cc642591fa5fff231cbddd9c40915eabb
Anonymous-BCFED <nope@no.pe>
2015-09-21 01:14:43 -0700

    Fix FlashDevelop support
    I develop with Flash CS6 + FlashDevelop since I hate Creative Cloud.
    
    # Changes
    
     * Use %LOCALAPPDATA% to locate user's Local AppData directory instead of a hardcoded directory. Works on Windows 7, at least.
     * Update to Flex SDK 5.6.0 + 18.0.0 (latest available for FD)
     * Fix a compile error possibly caused by upgrade.  Please yell if this is an intentional stumbling block.
    
    # Caveats
    
     * Still requires users to manually specify location of their Adobe Flash libraries since FD is dumb.

--- bat/SetupSDK.bat
+++ bat/SetupSDK.bat
@@ -1,7 +1,7 @@
 :user_configuration
 
 :: Path to Flex SDK
-set FLEX_SDK=C:\Users\Gedan\AppData\Local\FlashDevelop\Apps\flexairsdk\4.6.0+16.0.0
+set FLEX_SDK=%LOCALAPPDATA%\FlashDevelop\Apps\flexairsdk\4.6.0+18.0.0
 set AUTO_INSTALL_IOS=yes
 
 :: Path to Android SDK
